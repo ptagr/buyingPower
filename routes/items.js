d64431264f857315b97f76e2e1225759ab2eaeb9
punagrawal <punagrawal@ebay.com>
2014-11-24 14:45:04 -0800

    test

--- routes/items.js
+++ routes/items.js
@@ -5,6 +5,9 @@ var _ = require('underscore')._;
 
 
 var requestURL = 'http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=hackatha-b572-420a-8b2c-229be6d4a6b7&OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=10&itemFilter(0).name=ListingType&itemFilter.value=FixedPrice&categoryId=9355&outputSelector=SellerInfo';
+
+var getItemURL = 'ttp://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=hackatha-b572-420a-8b2c-229be6d4a6b7&siteid=0&version=515&IncludeSelector=Details&';
+
 router.route('/')
     .get(function (req, res) {
         request({
@@ -20,24 +23,52 @@ router.route('/')
 
                 var responsearray =[];
                 _.each(itemArray, function(item){
-                    var responseObject = {};
-                    responseObject.iteminfo  = {
-                        id : item.itemId[0],
-                        title : item.title[0],
-                        image : item.galleryURL[0],
-                        condition : item.condition[0].conditionDisplayName[0],
-                        price : item.sellingStatus[0].currentPrice[0].__value__
-                        //shippingInfo : item.shippingInfo[0]
-                    };
-                    responseObject.sellerinfo = {
-                        name : item.sellerInfo[0].sellerUserName[0],
-                        link : 'http://www.ebay.com/usr/'+item.sellerInfo[0].sellerUserName[0],
-                        feedbackScore : item.sellerInfo[0].feedbackScore[0],
-                        positiveFeedbackPercent : item.sellerInfo[0].positiveFeedbackPercent[0]
 
-                    }
-                    responsearray.push(responseObject);
+                    var itemURL = getItemURL+'ItemID='+item.itemId[0];
+                    request({
+                        url: itemURL,
+                        json: true
+                    }, function(error2, response2, body2){
+
+
+                        if (!error2 && response2.statusCode === 200) {
+                            var responseObject = {};
+                            var itm = body.Item;
+
+
+                            responseObject.iteminfo  = {
+                                id : itm.ItemID,
+                                title : itm.Title,
+                                image : itm.PictureURL[0],
+                                condition : 'New',
+                                price : itm.CurrentPrice.Value
+                                //shippingInfo : item.shippingInfo[0]
+                            };
+
+                            responseObject.sellerinfo = {
+                                name : itm.Seller.UserID,
+                                link : 'http://www.ebay.com/usr/'+itm.Seller.UserID,
+                                feedbackScore : itm.Seller.UserID.FeedbackScore,
+                                positiveFeedbackPercent : itm.Seller.UserID.PositiveFeedbackPercent
+
+                            };
+
+                            responseObject.quantityInfo = {
+                                sold : itm.QuantitySold,
+                                availableHint : itm.QuantityAvailableHint,
+                                availableThreshold : QuantityThreshold
+                            };
+
+                            responsearray.push(responseObject);
+                        }
+
+                    });
+
+
+
+
                 });
+
                 res.send(responsearray);
 
             }
