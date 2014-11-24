da5b401deb6e16935112026b4dbb724ea68b55c3
punagrawal <punagrawal@ebay.com>
2014-11-24 15:06:02 -0800

    add cors header and quantity sold

--- routes/items.js
+++ routes/items.js
@@ -6,7 +6,7 @@ var _ = require('underscore')._;
 
 var requestURL = 'http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=hackatha-b572-420a-8b2c-229be6d4a6b7&OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=10&itemFilter(0).name=ListingType&itemFilter.value=FixedPrice&categoryId=9355&outputSelector=SellerInfo';
 
-var getItemURL = 'ttp://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=hackatha-b572-420a-8b2c-229be6d4a6b7&siteid=0&version=515&IncludeSelector=Details&';
+var getItemURL = 'http://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=hackatha-b572-420a-8b2c-229be6d4a6b7&siteid=0&version=515&IncludeSelector=Details&';
 
 router.route('/')
     .get(function (req, res) {
@@ -20,6 +20,7 @@ router.route('/')
                 //res.send(body.findItemsByKeywordsResponse[0].searchResult[0].item);
 
                 var itemArray = body.findItemsAdvancedResponse[0].searchResult[0].item;
+                var ajaxCallsRemaining = 10;
 
                 var responsearray =[];
                 _.each(itemArray, function(item){
@@ -33,7 +34,7 @@ router.route('/')
 
                         if (!error2 && response2.statusCode === 200) {
                             var responseObject = {};
-                            var itm = body.Item;
+                            var itm = body2.Item;
 
 
                             responseObject.iteminfo  = {
@@ -56,10 +57,19 @@ router.route('/')
                             responseObject.quantityInfo = {
                                 sold : itm.QuantitySold,
                                 availableHint : itm.QuantityAvailableHint,
-                                availableThreshold : QuantityThreshold
+                                availableThreshold : itm.QuantityThreshold
                             };
 
                             responsearray.push(responseObject);
+
+                            --ajaxCallsRemaining;
+                            if (ajaxCallsRemaining <= 0) {
+                                // all data is here now
+                                // look through the returnedData and do whatever processing
+                                // you want on it right here
+                                res.header('Access-Control-Allow-Credentials','true')
+                                res.send(responsearray);
+                            }
                         }
 
                     });
@@ -69,7 +79,7 @@ router.route('/')
 
                 });
 
-                res.send(responsearray);
+                //res.send(responsearray);
 
             }
         });
