90eba1a46acd70f6d7e0c7170faa0dbfe3665519
punagrawal <punagrawal@ebay.com>
2014-11-24 16:27:56 -0800

    make server fast

--- routes/items.js
+++ routes/items.js
@@ -4,84 +4,74 @@ var request = require("request");
 var _ = require('underscore')._;
 
 
-var requestURL = 'http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=hackatha-b572-420a-8b2c-229be6d4a6b7&OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=10&itemFilter(0).name=ListingType&itemFilter.value=FixedPrice&categoryId=11450&outputSelector=SellerInfo';
+var requestURL = 'http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=hackatha-b572-420a-8b2c-229be6d4a6b7&OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=9&itemFilter(0).name=ListingType&itemFilter.value=FixedPrice&categoryId=11450&outputSelector=SellerInfo';
+
+var itemArray = ['161495451444', '381064177054', '271659087994', '191424275863', '321596068196', '261673779913', '271681650066',
+    '111526153929', '221614920705'];
 
 var getItemURL = 'http://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=hackatha-b572-420a-8b2c-229be6d4a6b7&siteid=0&version=515&IncludeSelector=Details&';
 
 router.route('/')
     .get(function (req, res) {
-        request({
-            url: requestURL,
-            json: true
-        }, function (error, response, body) {
 
-            if (!error && response.statusCode === 200) {
-                console.log(body) // Print the json response
-                //res.send(body.findItemsByKeywordsResponse[0].searchResult[0].item);
+        var ajaxCallsRemaining = 9;
 
-                var itemArray = body.findItemsAdvancedResponse[0].searchResult[0].item;
-                var ajaxCallsRemaining = 10;
+        var responsearray = [];
+        _.each(itemArray, function (item) {
 
-                var responsearray =[];
-                _.each(itemArray, function(item){
-
-                    var itemURL = getItemURL+'ItemID='+item.itemId[0];
-                    request({
-                        url: itemURL,
-                        json: true
-                    }, function(error2, response2, body2){
+            var itemURL = getItemURL + 'ItemID=' + item;
+            request({
+                url: itemURL,
+                json: true
+            }, function (error2, response2, body2) {
 
 
-                        if (!error2 && response2.statusCode === 200) {
-                            var responseObject = {};
-                            var itm = body2.Item;
+                if (!error2 && response2.statusCode === 200) {
+                    var responseObject = {};
+                    var itm = body2.Item;
 
 
-                            responseObject.iteminfo  = {
-                                id : itm.ItemID,
-                                title : itm.Title,
-                                image : itm.PictureURL[0],
-                                condition : 'New',
-                                price : itm.CurrentPrice.Value
-                                //shippingInfo : item.shippingInfo[0]
-                            };
+                    responseObject.iteminfo = {
+                        id: itm.ItemID,
+                        title: itm.Title,
+                        image: itm.PictureURL[0],
+                        condition: 'New',
+                        price: itm.CurrentPrice.Value
+                        //shippingInfo : item.shippingInfo[0]
+                    };
 
-                            responseObject.sellerinfo = {
-                                name : itm.Seller.UserID,
-                                link : 'http://www.ebay.com/usr/'+itm.Seller.UserID,
-                                feedbackScore : itm.Seller.UserID.FeedbackScore,
-                                positiveFeedbackPercent : itm.Seller.UserID.PositiveFeedbackPercent
+                    responseObject.sellerinfo = {
+                        name: itm.Seller.UserID,
+                        link: 'http://www.ebay.com/usr/' + itm.Seller.UserID,
+                        feedbackScore: itm.Seller.UserID.FeedbackScore,
+                        positiveFeedbackPercent: itm.Seller.UserID.PositiveFeedbackPercent
 
-                            };
+                    };
 
-                            responseObject.quantityInfo = {
-                                sold : itm.QuantitySold,
-                                availableHint : itm.QuantityAvailableHint,
-                                availableThreshold : itm.QuantityThreshold
-                            };
+                    responseObject.quantityInfo = {
+                        sold: itm.QuantitySold,
+                        availableHint: itm.QuantityAvailableHint,
+                        availableThreshold: itm.QuantityThreshold
+                    };
 
-                            responsearray.push(responseObject);
+                    responsearray.push(responseObject);
+                    console.log("hello" + ajaxCallsRemaining);
+                    --ajaxCallsRemaining;
+                    if (ajaxCallsRemaining <= 0) {
+                        // all data is here now
+                        // look through the returnedData and do whatever processing
+                        // you want on it right here
+                        res.send(responsearray);
+                    }
+                }
 
-                            --ajaxCallsRemaining;
-                            if (ajaxCallsRemaining <= 0) {
-                                // all data is here now
-                                // look through the returnedData and do whatever processing
-                                // you want on it right here
-                                res.send(responsearray);
-                            }
-                        }
-
-                    });
-
-
+            });
 
 
-                });
-
-                //res.send(responsearray);
-
-            }
         });
+
+        //res.send(responsearray);
+
     });
 
 module.exports = router;
