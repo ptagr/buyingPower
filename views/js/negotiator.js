72cf4aef4950ea19bd6f589c92dee94b1d877190
punagrawal <punagrawal@ebay.com>
2014-11-24 22:50:39 -0800

    add cat

--- views/js/negotiator.js
+++ views/js/negotiator.js
@@ -4,8 +4,23 @@
         var itemArr = [],
             itemGridHTML = '';
 
+        function getUrlVars()
+        {
+            var vars = [], hash;
+            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+            for(var i = 0; i < hashes.length; i++)
+            {
+                hash = hashes[i].split('=');
+                vars.push(hash[0]);
+                vars[hash[0]] = hash[1];
+            }
+            return vars;
+        }
+
+        var urlVars = getUrlVars();
+
         $.ajax({
-            url: 'https://protected-oasis-8857.herokuapp.com/items',
+            url: 'https://protected-oasis-8857.herokuapp.com/items?catid='+urlVars['catid'],
             // url: 'http://localhost:3000/items',
             type: 'GET',
             // dataType: 'json',
