9dc2755085d66ecf1ed6e73c1dc15a6cbedb3ecf
punagrawal <punagrawal@ebay.com>
2014-11-24 23:59:17 -0800

    integrate deval changes

--- routes/optin.js
+++ routes/optin.js
@@ -10,7 +10,7 @@ exports.optin = function(req, res) {
 function getResponse(itemid, res) {
 	var count =0;
 	request({
-        url: "http://protected-oasis-8857.herokuapp.com/getoffer?itemid=381064177054",
+        url: "http://protected-oasis-8857.herokuapp.com/getoffer?itemid="+itemid,
         json: true
     }, function (error2, response2, body2) {
     	console.log(body2);
