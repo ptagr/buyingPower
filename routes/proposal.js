e2638f106e5a8e5f35af2cf782e50e87aa791dbf
punagrawal <punagrawal@ebay.com>
2014-11-24 18:22:07 -0800

    mail module

--- routes/proposal.js
+++ routes/proposal.js
@@ -2,6 +2,26 @@ var express = require('express');
 var router = express.Router();
 var request = require("request");
 var _ = require('underscore')._;
+var postmark = require("postmark")(process.env.POSTMARK_API_KEY)
+
+router.route('/')
+    .get(function (req, res) {
+        postmark.send({
+            "From": "update@ebayNegotiator.com",
+            "To": "punit21@outlook.com",
+            "Subject": "Hello from Postmark",
+            "TextBody": "Hello!",
+            "Tag": "big-bang"
+        }, function(error, success) {
+            if(error) {
+                console.error("Unable to send via postmark: " + error.message);
+                return;
+            }
+            console.info("Sent to postmark for delivery")
+        });
+       res.send()
+    });
+
 
 
 module.exports = router;
