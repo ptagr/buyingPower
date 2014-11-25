fa1edf7afe25a1c4c0ba1f6ff209aec4360d6691
punagrawal <punagrawal@ebay.com>
2014-11-24 16:13:51 -0800

    fix cors

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ var users = require('./routes/users');
 var items = require('./routes/items')
 
 
+// Enables CORS
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    next();
+};
+
 var app = express();
 
 // view engine setup
@@ -62,14 +71,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-// Enables CORS
-var enableCORS = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-        next();
-};
 
 
 module.exports = app;
