caacfcac2c50d4b1f346a3ba4edd6800f4896d73
punagrawal <punagrawal@ebay.com>
2014-11-24 18:52:45 -0800

    mail module

--- routes/proposal.js
+++ routes/proposal.js
@@ -5,13 +5,27 @@ var _ = require('underscore')._;
 var postmark = require("postmark")(process.env.POSTMARK_API_KEY);
 
 
+
+
+
 router.route('/')
     .get(function (req, res) {
+
+        var emailText = 'Hi '+ req.query.sellerName + ',\n'+
+            'Negotiator ' + req.query.negotiatorName + ' has created a group deal for your item '+ req.query.itemid + '.\n'+
+            'Please choose to Accept or Reject after reviewing the proposal as detailed below:'+'\n' +
+            'Quantity: '+ req.query.quantity +'\n' +
+            'Duration: '+ req.query.duration +'\n' +
+            'Discount: '+ req.query.discount +'\n' +
+            'ACCEPT	REJECT'+ '\n\n'+
+            'Thanks,'+ '\n'+
+            'eBay BuyingPower Team';
+        console.log(emailText);
         postmark.send({
             "From": "PUNAGRAWAL@EBAY.COM",
             "To": "punit21@outlook.com",
-            "Subject": "Hello from Postmark",
-            "TextBody": "Hello!",
+            "Subject": "You have received a new Negotiator Interest !!",
+            "TextBody": emailText,
             "Tag": "big-bang"
         }, function(error, success) {
             if(error) {
