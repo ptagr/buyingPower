17983ef86f82d208d60c73bbb2a4f061ad821102
punagrawal <punagrawal@ebay.com>
2014-11-24 23:35:03 -0800

    integrate deval changes

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(enableCORS);
 
+app.use('/', routes);
 app.use('/users', users);
 app.use('/items', items);
 app.use('/proposal', proposal);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,104 @@
+var express = require('express');
+var router = express.Router();
+var url = require('url');
+var app = require('../app');
+var mongoose = require('mongoose');
 
-/*
- * GET home page.
- */
+var mongoURL = process.env.MONGOSOUP_URL;
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+/* GET home page. */
+router.get('/', function(req, res) {
+    mongoose.connect(mongoURL, function(error, db){
+        if(error) {
+            console.log("There is an error");
+        }
+        var offerCollection = db.collection('test_collection');
+
+        var offer1 = new offerCollection({
+            sellerName : 'mountainsrgreat2010',
+            sellerFeedback :527,
+            itemId : '381064177054',
+            title: 'Sandy Starkman hippie boho floral sundress-Larg...',
+            imgUrl : "http://i.ebayimg.com/00/s/MTYwMFgxMjAw/z/~OUAAOSwAL9UcqcQ/$_1.JPG?set_id=880000500F",
+            itemDesc : " This is an awesome movie",
+            listPrice : 20,
+            offerPrice: 10,
+            savingPrice:10,
+            boughtQty:5,
+            totalQty:100
+        });
+
+        offer1.save(function(err, offerValue) {
+            if (err) return console.error(err);
+            console.log(offerValue);
+        });
+
+        offerCollection.find(function(err, thor) {
+            if (err) return console.error(err);
+            res.send(thor);
+        });
+
+    });
+
+
+
+});
+
+router.get('/getOffer*', function(req,res){
+    var itemIdVar = req.param("itemid");
+
+    mongoose.connect(mongoURL, function(error, DB){
+        if(error) {
+            console.log("There is an error");
+        }
+        var offerCollection = db.collection('test_collection');
+
+        offerCollection.findOne({ itemId: itemIdVar}, function(err, thor) {
+            if (err) return console.error(err);
+            res.send(thor);
+            console.log(thor);
+        });
+    });
+
+
+
+});
+
+router.get('/updateOffer*', function(req,res){
+    var itemIdVar = req.param("itemid");
+
+    mongoose.connect(mongoURL, function(error, db){
+        if(error) {
+            console.log("There is an error");
+        }
+        var offerCollection = db.collection('test_collection');
+        offerCollection.update(
+            {itemId: itemIdVar},
+            { $inc: { boughtQty: -1} }, function(error, updatedValue){
+                if(error) {
+                    console.log("There is an error");
+                }
+
+                offerCollection.findOne({ itemId: itemIdVar}, function(err, thor) {
+                    if (err) return console.error(err);
+                    res.send(thor);
+                    console.log(thor);
+                });}
+        );
+    });
+
+
+
+});
+
+router.get('/submitjson*', function(req, res) {
+    console.log("Hi");
+    var serviceReq = req.body.xorequest;
+    console.log(serviceReq);
+    console.log(req.method);
+    res.send(resString);
+    // res.redirect('/');
+    res.end();
+});
+
+module.exports = router;
